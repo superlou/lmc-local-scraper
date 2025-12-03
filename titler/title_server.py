8bbede002e2b4c6efbfc585689b07ec72b108820
superlou <lousimons@gmail.com>
2025-12-03 09:08:36 -0500

    Fixed content types

--- titler/title_server.py
+++ titler/title_server.py
@@ -28,20 +28,29 @@ class TitleServerRequestHandler(server.BaseHTTPRequestHandler):
         parsed_url = urllib.parse.urlparse(self.path)
         query = urllib.parse.parse_qsl(parsed_url.query)
         file_path = root / parsed_url.path[1:]  # Remove leading "/"
-        print(query)
 
         if file_path.exists():
+            print(f"found html: {file_path}")
             file_contents = open(file_path, "rb").read()
-        elif template_path := file_path.with_suffix(file_path.suffix + ".jinja2"):
+        elif (
+            template_path := file_path.with_suffix(file_path.suffix + ".jinja2")
+        ).exists():
             template_contents = open(template_path).read()
             template = Template(template_contents)
-            print(template)
             file_contents = template.render(text="something").encode()
         else:
             self.send_error(404, f"Path not found: {self.path}")
             return
 
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+
+        if file_path.suffix == ".html":
+            content_type = "text/html"
+        elif file_path.suffix == ".css":
+            content_type = "text/css"
+        else:
+            content_type = "text"
+
+        self.send_header("Content-type", content_type)
         self.end_headers()
         self.wfile.write(file_contents)
